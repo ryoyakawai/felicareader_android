ce44ee712eddb2a53159e2c3d04fbb51bbe78c10
Ryoya Kawai <ryoya.kawai@gmail.com>
2020-05-24 15:57:48 +0900

    copy & paste

--- app/src/main/java/com/ryoyakawai/felicareader/MainActivity.kt
+++ app/src/main/java/com/ryoyakawai/felicareader/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.ryoyakawai.felicareader
 
 import android.Manifest
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.NfcF
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-//import com.ryoyakawai.felicareader.R
+
 import com.ryoyakawai.felicareader.api.response.SinglePostResponse
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.ryoyakawai.felicareader.libs.FelicaReader
 
 class MainActivity : AppCompatActivity(),
     MainActivityViewContract {
@@ -27,18 +36,65 @@ class MainActivity : AppCompatActivity(),
     private var permissionDeniedBehavior: ((activity: MainActivity) -> Unit ) = {}
     private var permissionUndefinedBehavior: ((activity: MainActivity) -> Unit ) = {}
 
+    // // vvvv felica vvvv
+    private var intentFiltersArray: Array<IntentFilter>? = null
+    // FelicaはNFC-TypeFなのでNfcFのみ指定
+    private val techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
+    private val nfcAdapter: NfcAdapter? by lazy {
+        NfcAdapter.getDefaultAdapter(this)
+    }
+    private var pendingIntent: PendingIntent? = null
+    private val felicaReader = FelicaReader()
+    // // ^^ ^^ felica ^^ ^^
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         this.mPresenter = MainActivityPresenter()
         this.mPresenter!!.setView(this)
-
-        setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar)
 
         updateMainContentText("Hello World!!")
+
+        // // vvvv felica vvvv
+        pendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+        )
+
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+
+        try {
+            ndef.addDataType("text/plain")
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            throw RuntimeException("fail", e)
+        }
+
+        intentFiltersArray = arrayOf(ndef)
+
+        if (nfcAdapter != null) {
+            //NFCが搭載されてない端末
+            val builder = AlertDialog.Builder(this@MainActivity, R.style.NfcAlertDialogStyle00)
+            builder.setMessage("サービス対象外です")
+            builder.setPositiveButton("キャンセル", null)
+        } else if (!nfcAdapter!!.isEnabled) {
+            //NFCが無効になっている時
+            val builder = AlertDialog.Builder(this@MainActivity, R.style.NfcAlertDialogStyle00)
+            builder.setTitle("NFC無効")
+            builder.setMessage("NFCを有効にしてください")
+            builder.setPositiveButton("設定") { _, _ -> startActivity(Intent(Settings.ACTION_NFC_SETTINGS)) }
+            builder.setNegativeButton("キャンセル", null)
+
+            val myDialog = builder.create()
+            //ダイアログ画面外をタッチされても消えないようにする。
+            myDialog.setCanceledOnTouchOutside(false)
+            //ダイアログ表示
+            myDialog.show()
+        }
+        getTag(intent)
+        // // ^^^^ Felica ^^^^
 
         val emailFab: FloatingActionButton = findViewById(R.id.email_fab)
         emailFab.setOnClickListener { view ->
@@ -74,6 +130,17 @@ class MainActivity : AppCompatActivity(),
 
             handleOkButton(view)
         }
+    }
+
+    private fun getTag(intent: Intent) {
+        // IntentにTagの基本データが入ってくるので取得。
+        val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG) ?: return
+        felicaReader.readTag(tag, applicationContext)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        getTag(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
